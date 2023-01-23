3bfe4d97b12cf8940cb78c79b0c3bd4216d3f688
Tom MacKenzie <tom.mackenzie@sky.uk>
2023-01-23 17:04:36 +0000

    Add a group for all Jackson dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,9 @@ import sbt._
 object Dependencies {
   val deps = Seq(
     "org.scalatest" %% "scalatest" % "3.2.7" % "test",
-    "com.lihaoyi" %% "os-lib" % "0.7.0"
+    "com.lihaoyi" %% "os-lib" % "0.7.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.11.4",
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.2",
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.11.3"
   )
 }
