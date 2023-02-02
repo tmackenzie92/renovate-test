59c46f335d4a5b7af64634765e50ce636f013ad2
Tom MacKenzie <tom.mackenzie@sky.uk>
2023-02-02 10:53:45 +0000

    Add Cucumber dependencies and Renovate group

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,8 @@ object Dependencies {
     "com.lihaoyi" %% "os-lib" % "0.7.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.11.4",
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.2",
-    "com.fasterxml.jackson.core" % "jackson-core" % "2.11.3"
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.11.3",
+    "io.cucumber" %% "cucumber-scala" % "4.7.1",
+    "io.cucumber" % "cucumber-junit" % "4.7.1"
   )
 }
