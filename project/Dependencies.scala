de4db4d6c28d1ffde60a66fb9929c07e0d44534c
tmackenzie92 <tom.mackenzie@sky.uk>
2023-07-14 12:07:40 +0100

    Add play-json dependency to test ignoring multiple dependencies via environment variable

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,6 +23,7 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % "3.2.7" % "test",
     "com.lihaoyi" %% "os-lib" % "0.7.0",
     "io.cucumber" %% "cucumber-scala" % "4.7.1",
-    "io.cucumber" % "cucumber-junit" % "4.7.1"
+    "io.cucumber" % "cucumber-junit" % "4.7.1",
+    "com.typesafe.play" %% "play-json" % "2.7.1"
   )
 }
